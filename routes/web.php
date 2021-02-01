eae399d3fafa43bc67bd5530303885190fb93493
Abdul Rahman <abdul@foomotion.io>
2021-02-01 19:32:06 +0500

    creating laravel project with bootstrap template

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
