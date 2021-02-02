ee319e03a6131f1d8d9fbe059cf8588ade1b1fbc
Afzal Ali <afzala497@gmail.com>
2021-02-02 13:47:55 +0500

    create factories for dummy data, add scripts for datatables and create page to show posts of user and sidebar links to create and view user posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\User;
 
 class PostController extends Controller
 {
     //
 
-    public function show(){
-        return view('blog-post');
+    public function index(){
+        
+        $posts = Post::all();
+
+        return view('admin.posts.index', ['posts'=>$posts]);
+    }
+    public function show(Post $post){
+        // as we are getting class object in param
+        // so even by passing post id, it automatically find the 
+        // post in post variable
+        // Post::findOrFail($id);
+        return view('blog-post',['post'=>$post]);
+    }
+    public function create(){
+        
+        return view('admin.posts.create');
+    }
+
+    public function store(){
+        // validation of form ,so user can't submit empty form
+        $inputs = request()->validate([
+            // define rules here for validations
+            'title'=>'required|min:8|max:255',
+            'post_image'=>'file', // for diff formats
+            'body'=>'required'
+            // 'image'=>'mimes:jpeg,png,', // for diff formats
+        ]);
+
+        if(request('post_image')){
+            $inputs['post_image'] = request('post_image')->store('images');
+        }
+
+        auth()->user()->posts()->create($inputs);
+
+        // dd(request('image'));  // show data of image
+        return back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,19 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
-Route::get('/post', [App\Http\Controllers\PostController::class, 'show'])->name('post');
+Route::get('/post/{post}', [App\Http\Controllers\PostController::class, 'show'])->name('post');
+
+
+
+// this is auth middleware any function inside this require to pass
+// middle ware otherwise redirect to login page or whatever page mentionedby 
+// middleware
+Route::middleware('auth')->group(function(){
+
+    Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
+    // post
+    Route::get('/admin/posts/create',[App\Http\Controllers\PostController::class,'create'])->name('posts.create');
+    Route::post('/admin/posts',[App\Http\Controllers\PostController::class,'store'])->name('posts.store');
+    Route::get('/admin/posts',[App\Http\Controllers\PostController::class,'index'])->name('posts.index');
+
+});
